5370bcaa34bc4859d4c12e5872084aaf631d1f0c
TanY <tankh2o@yandex.ru>
2023-02-27 10:03:04 +0800

    2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Задачи 1 и 2");
+        //нет комментариев
             String firstName = "Ivan";
             String middleName = "Ivanovich";
             String lastName = "Ivanov";
